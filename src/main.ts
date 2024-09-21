5f1ed6a319799fb8bbffb8b561aae085c1b28910
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-09-21 17:11:26 -0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,17 @@ import { getSecretValue } from './loadsecrets';
 require('dotenv').config();
 
 async function bootstrap() {
+  try {
+    var secrets = getSecretValue()
+    for (const key in secrets) {
+      process.env[key] = secrets[key];
+    }
+    console.log(secrets)
+    console.log('Segredos carregados com sucesso!');
+    } catch (err) {
+      console.error('Erro ao carregar os segredos:', err);
+    }
+  
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter());
@@ -21,17 +32,8 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  try {
-    var secrets = getSecretValue()
-    for (const key in secrets) {
-      process.env[key] = secrets[key];
-    }
-    console.log(secrets)
-    console.log('Segredos carregados com sucesso!');
-    await app.listen(3000);
-  } catch (err) {
-    console.error('Erro ao carregar os segredos:', err);
-  }
+
+  await app.listen(3000);
   
 }
 
