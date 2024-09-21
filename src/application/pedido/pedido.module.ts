9bd98a051f4847b2d2d3d2a4e5f6b0f78b7e6bb7
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-09-20 21:05:31 -0300

    remove sql configuration

--- src/application/pedido/pedido.module.ts
+++ src/application/pedido/pedido.module.ts
@@ -1,15 +1,11 @@
 import { Module } from "@nestjs/common";
 import { IPedidoRepository } from "src/domain/pedido/repositories/order-repository.interface";
-import { databaseProviders } from "src/infrastructure/data/database.provider";
 import { OrderController } from "./controller/pedido.controller";
 import { OrderMapper } from "./mapper/pedido.mapper";
 import { ProductRepository } from "src/infrastructure/data/produto/repositories/produto-repository";
 import { IProdutoRepository } from "src/domain/produto/repositories/product-repository.interface";
-import { produtoProviders } from "src/infrastructure/data/produto/repositories/produto.provider";
-import { categoriaProviders } from "src/infrastructure/data/produto/repositories/categoria.provider";
 import { ProdutoUseCase } from "src/domain/produto/use-cases/produto-use-case.service";
 import { IProdutoUseCase } from "src/domain/produto/use-cases/produto-use-case.interface";
-import { pedidoProviders } from "src/infrastructure/data/pedido/repositories/pedido.provider";
 import { PedidoRepository } from "src/infrastructure/data/pedido/repositories/pedido.repository";
 import { PedidoUseCase } from "src/domain/pedido/use-cases/pedido-use-case.service";
 import { IPedidoUseCase } from "src/domain/pedido/use-cases/pedido-use-case.interface";
@@ -19,8 +15,6 @@ import { IPedidoUseCase } from "src/domain/pedido/use-cases/pedido-use-case.inte
     controllers: [OrderController],
     providers: [
         OrderMapper,
-        ...pedidoProviders,
-        ...databaseProviders,
         PedidoUseCase,
         {
             provide: IPedidoUseCase,
@@ -31,8 +25,6 @@ import { IPedidoUseCase } from "src/domain/pedido/use-cases/pedido-use-case.inte
             provide: IPedidoRepository,
             useClass: PedidoRepository
         },
-        ...produtoProviders,
-        ...categoriaProviders,
         ProdutoUseCase,
         {
             provide: IProdutoUseCase,
--- src/infrastructure/data/cliente/repositories/cliente-repository.ts
+++ src/infrastructure/data/cliente/repositories/cliente-repository.ts
@@ -1,50 +1,21 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { Cliente } from "src/domain/cliente/entities/cliente.entity";
 import { IClienteRepository } from "src/domain/cliente/repositories/cliente-repository.interface";
-import { Repository } from "typeorm";
-import { Clientes } from "../entities/customer.entity";
 
 @Injectable()
 export class ClienteRepository implements IClienteRepository  {
     constructor(
-        @Inject('CLIENTE_REPOSITORY')
-        private clienteRepo: Repository<Clientes>,
+       
       ) {}
     
     async getAll(): Promise<Cliente[]> {
-        const clientes = await this.clienteRepo
-        .createQueryBuilder("Clientes")
-        .getMany()
-
-        const clientesList: Cliente[] = []
-
-        clientes.forEach(c => {
-            clientesList.push(new Cliente(c.ClienteNome, c.ClienteDocumento, c.Email))
-        })
-
-        return clientesList
+        return null 
     }
     
     async getByCpf(cpf: string): Promise<Cliente | undefined> {
-        const customers = await this.clienteRepo
-        .createQueryBuilder("Clientes")
-        .where("Clientes.ClienteDocumento = :document", { document: cpf })
-        .getOne()
-        
-        if (customers === null) return undefined;
-        const customer = new Cliente(customers.ClienteNome, customers.ClienteDocumento, customers.Email);
-        return customer
+        return null
     }
     
     create(customer: Cliente) {
-        const customerEntity = new Clientes()
-        customerEntity.ClienteId = customer.id;
-        customerEntity.ClienteNome = customer.nome;
-        customerEntity.ClienteDocumento = customer.cpf.numero;
-        customerEntity.TipoDocumento = "CPF";
-        customerEntity.Email = customer.email;
-
-        this.clienteRepo.create(customerEntity);
-        this.clienteRepo.save(customerEntity);
     }
 }
