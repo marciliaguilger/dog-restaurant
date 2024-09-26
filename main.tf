2c8f09c4c895d55b08ef959ced7670d7ad66fbe7
beatriz.lorente <beatriz.lorente@ifood.com.br>
2024-09-26 16:51:58 -0300

    feat: Update Terraform

--- main.tf
+++ main.tf
@@ -101,3 +101,54 @@ resource "aws_lb_listener" "http_listener" {
     target_group_arn = aws_lb_target_group.dog_tg.arn
   }
 }
+
+# Create an API Gateway HTTP API
+resource "aws_apigatewayv2_api" "dog_restaurant_api" {
+  name          = "dog-restaurant-api"
+  protocol_type = "HTTP"
+}
+
+# Create an API Gateway Integration
+resource "aws_apigatewayv2_integration" "http_integration" {
+  api_id           = aws_apigatewayv2_api.dog_restaurant_api.id
+  integration_type = "HTTP_PROXY"
+  integration_uri  = aws_lb_listener.http_listener.arn
+  integration_method = "ANY"
+  connection_type  = "VPC_LINK"
+  connection_id    = aws_apigatewayv2_vpc_link.vpc_link.id
+  payload_format_version = "1.0"
+}
+
+# Create an API Gateway Authorizer
+resource "aws_apigatewayv2_authorizer" "authorizer" {
+  api_id = aws_apigatewayv2_api.dog_restaurant_api.id
+  authorizer_type = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+  name = "dog-authorizer"
+  jwt_configuration {
+    audience = ["sce9h2dp1ia781q6to7i4u6np"]
+    issuer = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Z5ZdvqJFj"
+  }
+}
+# Create an API Gateway Route
+resource "aws_apigatewayv2_route" "http_route" {
+  api_id    = aws_apigatewayv2_api.dog_restaurant_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.http_integration.id}"
+  authorizer_id = aws_apigatewayv2_authorizer.authorizer.id
+  authorization_type = "JWT"
+}
+
+# Create an API Gateway Stage
+resource "aws_apigatewayv2_stage" "http_stage" {
+  api_id      = aws_apigatewayv2_api.dog_restaurant_api.id
+  name        = "default"
+  auto_deploy = true
+}
+
+# Create a VPC Link
+resource "aws_apigatewayv2_vpc_link" "vpc_link" {
+  name = "dog-restaurant-vpc-link"
+  subnet_ids = ["subnet-08bf99e4f496868fb", "subnet-0c18fc34912b5cde3"]
+  security_group_ids = ["sg-0b804dd8ca3753013"]
+}
